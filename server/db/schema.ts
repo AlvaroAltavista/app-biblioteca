2eb84715f64c0d690faf7d37f4b16fbc5691d1bf
AlvaroAltavista <76492165+AlvaroAltavista@users.noreply.github.com>
2026-06-15 17:50:51 +0200

    config: modificaciones de configuración

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,1 +1,43 @@
-import { sqliteTable, integer } from "drizzle-orm/sqlite-core";
+import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
+import { defineConfig } from "drizzle-kit";
+
+// Config
+export default defineConfig({
+	dialect: "sqlite",
+	schema: "@/schema/db/schema.ts",
+});
+
+// ============================
+// Tablas Maestras
+// ============================
+export const genero = sqliteTable("genero", {
+	id: integer().primaryKey({ autoIncrement: true }),
+	nombre: text("nombre").notNull().unique(),
+});
+
+export const subGenero = sqliteTable("subGenero", {
+	id: integer().primaryKey({ autoIncrement: true }),
+	nombre: text("nombre").notNull().unique(),
+});
+
+export const coleccion = sqliteTable("coleccion", {
+	id: integer().primaryKey({ autoIncrement: true }),
+	nombre: text("nombre").notNull().unique(),
+});
+
+// ============================
+// Entidades
+// ============================
+export const editorial = sqliteTable("editorial", {
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	nombre: text("nombre").notNull(),
+});
+
+export const libro = sqliteTable("libro", {
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	titulo: text("titulo").notNull(),
+});
