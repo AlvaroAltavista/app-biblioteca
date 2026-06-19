8f29bffe26a336140443addd8dbbeee3910e4774
AlvaroAltavista <76492165+AlvaroAltavista@users.noreply.github.com>
2026-06-19 21:49:20 +0200

    schema: desarrollo del schema de la db

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,4 +1,4 @@
-import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, integer, text, check } from "drizzle-orm/sqlite-core";
 import { defineConfig } from "drizzle-kit";
 
 // Config
@@ -10,34 +10,102 @@ export default defineConfig({
 // ============================
 // Tablas Maestras
 // ============================
-export const genero = sqliteTable("genero", {
+export const Genero = sqliteTable("Genero", {
 	id: integer().primaryKey({ autoIncrement: true }),
 	nombre: text("nombre").notNull().unique(),
 });
 
-export const subGenero = sqliteTable("subGenero", {
+export const SubGenero = sqliteTable("SubGenero", {
 	id: integer().primaryKey({ autoIncrement: true }),
 	nombre: text("nombre").notNull().unique(),
 });
 
-export const coleccion = sqliteTable("coleccion", {
+export const Coleccion = sqliteTable("Coleccion", {
 	id: integer().primaryKey({ autoIncrement: true }),
+	nombre: text("nombre").notNull().unique(),
+});
+
+export const Estado = sqliteTable("Estado", {
 	nombre: text("nombre").notNull().unique(),
 });
 
 // ============================
 // Entidades
 // ============================
-export const editorial = sqliteTable("editorial", {
+export const Editorial = sqliteTable("Editorial", {
 	id: text("id")
 		.primaryKey()
 		.$defaultFn(() => crypto.randomUUID()),
 	nombre: text("nombre").notNull(),
 });
 
-export const libro = sqliteTable("libro", {
+export const Libro = sqliteTable("Libro", {
 	id: text("id")
 		.primaryKey()
 		.$defaultFn(() => crypto.randomUUID()),
 	titulo: text("titulo").notNull(),
+	idGenero: integer("idGenero")
+		.references(() => Genero.id, { onDelete: "restrict" })
+		.notNull(),
+	idSubgenero: integer("idSubgenero").references(() => SubGenero.id, { onDelete: "restrict" }),
+	idEditorial: text("idEditorial").references(() => Editorial.id, { onDelete: "restrict" }),
+	isbn: text("isbn").notNull().unique(),
+	idColeccion: text("idColeccion").references(() => Coleccion.id, { onDelete: "restrict" }),
+	coverImgPath: text("coverImgPath"),
+	anyoEscritura: integer("anyoEscritura"),
+	anyoEdicion: integer("anyoEdicion"),
+	createdAt: integer({ mode: "timestamp" }).$defaultFn(() => new Date()),
+	updatedAt: integer({ mode: "timestamp" }).$defaultFn(() => new Date()),
+});
+
+export const Contacto = sqliteTable("Contacto", {
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	nombre: text("nombre").notNull().unique(),
+	metodoContacto: text("metodo_contacto").notNull().unique(),
+});
+
+export const Autor = sqliteTable("Autor", {
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	nombre: text("nombre").notNull(),
+	apellidos: text("apellidos"),
+});
+
+export const Usuario = sqliteTable("Usuario", {
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	nombre: text("nombre").notNull().unique(),
+	password: text("password").notNull(),
+});
+
+export const Nota = sqliteTable("Nota", {
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	idUsuario: text("idUsuario")
+		.references(() => Usuario.id)
+		.notNull(),
+	idLibro: text("idLibro")
+		.references(() => Libro.id)
+		.notNull(),
+	pagina: integer("pagina"),
+	texto: text("texto"),
+	createdAt: integer({ mode: "timestamp" }).$defaultFn(() => new Date()),
+	updatedAt: integer({ mode: "timestamp" }).$defaultFn(() => new Date()),
+});
+
+// ===============================
+// Tablas intermedias
+// ===============================
+
+export const LibroContacto = sqliteTable("LibroContacto", {
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	idLibro: text("idLibro").references(() => Libro.id, { onDelete: "cascade" }),
+	idContacto: text("idContacto").references(() => )
 });
